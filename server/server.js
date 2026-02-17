869b60773aec7dacb6ac1597f78929b8b26dcf2f
Mahi Prajapati <mahi.23bce10098@vitbhopal.ac.in>
2026-02-17 21:29:39 +0530

    Initialized Express backend setup with dependencies

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("SudokuVerse Backend Running 🚀");
+});
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
