048545ff867f24b899f5c46f71245f909f5ee08e
Mahi Prajapati <mahi.23bce10098@vitbhopal.ac.in>
2026-02-17 22:54:54 +0530

    Connected backend to MongoDB Atlas and configured environment setup

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
@@ -13,6 +14,10 @@ app.get("/", (req, res) => {
   res.send("SudokuVerse Backend Running 🚀");
 });
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected Successfully"))
+  .catch((err) => console.log("MongoDB Connection Error:", err));
 // Port
 const PORT = process.env.PORT || 5000;
 
